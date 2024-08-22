cdadd76c818de35145177326abce9e650c1b14bf
Naeema Majed <na3eemaa2001@gmail.com>
2024-08-22 17:07:01 +0300

    commit

--- src/main/java/com/embarkx/firstjobapp/Job/JobController.java
+++ src/main/java/com/embarkx/firstjobapp/Job/JobController.java
@@ -1,10 +1,11 @@
 package com.embarkx.firstjobapp.Job;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@RestController
 public class JobController {
     private List<Job> jobs=new ArrayList<>();
     @GetMapping("/jobs")
